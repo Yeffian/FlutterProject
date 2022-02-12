f7293dd8f539112037e2d5103b2063650f4c1a35
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-02-12 15:11:34 +0600

    inital commit

---
+++ android/app/src/main/kotlin/dev/yeff/learnflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.yeff.learnflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Color Picker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Color> _colors = {
+    Colors.purple,
+    Colors.red,
+    Colors.blue,
+    Colors.teal,
+    Colors.pink,
+    Colors.orange
+  } as List<Color>;
+
+  Color? selectedColor;
+
+  void _setColor(Color color) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('colorValue', color.value);
+
+    setState(() => {
+      selectedColor = color
+    });
+  }
+
+  void _getStoredColor() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? colorName = prefs.getInt('colorValue');
+
+    setState(() => {
+      selectedColor = Color(colorName as int)
+    });
+  }
+
+  @override
+  void initState() {
+    _getStoredColor();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: selectedColor,
+        title: const Text("Pog Color Switcher App"),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'You are on ${Platform.operatingSystem}',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white
+              ),
+            ),
+          ),
+          Center(
+            child: CircleColorPicker(
+              onChanged: (color) => _setColor(color),
+              size: const Size(240, 240),
+              strokeWidth: 4,
+              thumbSize: 36,
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
